c9767ee509f31719c1b99a2f0a4651843920316c
elen0ng <elenasitiaisya@gmail.com>
2023-06-14 21:45:18 +0700

    Menambah Create Sarana

---
+++ frontendpbo/CreateSarana.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frontendpbo
+{
+    public partial class CreateSarana : Form
+    {
+        public CreateSarana()
+        {
+            InitializeComponent();
+        }
+
+        private void simpan_Click(object sender, EventArgs e)
+        {
+            string nama_sektor = tbxNama.Text;
+            string deskripsi = tbxDeskripsi.Text;
+
+            dataGridView1.Rows.Add(nama_sektor, deskripsi);
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
