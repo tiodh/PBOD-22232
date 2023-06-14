8ae8f2a17e013a6f0f36f0246225b988aafd0b01
Lynxx39 <farisdhiyaul04@gmail.com>
2023-06-14 18:34:44 +0700

    CRUD dan DETAIL data Keamanan dan Keselamatan

---
+++ frontendpbo/detail_dataKeselamatan.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace frontendpbo
+{
+    partial class detail_dataKeselamatan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(detail_dataKeselamatan));
+            panel1 = new Panel();
+            textBox_deskripsiHospital = new TextBox();
+            textBox_alamatHospital = new TextBox();
+            textBox_tlpHospital = new TextBox();
+            textBox_lembagaHospital = new TextBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            textBox_deskripsiPolisi = new TextBox();
+            textBox_alamatPolisi = new TextBox();
+            textBox_hpPolisi = new TextBox();
+            textBox_lembagaPolisi = new TextBox();
+            pictureBox2 = new PictureBox();
+            panel3 = new Panel();
+            textBox_deskripsiBencana = new TextBox();
+            textBox_alamatBencana = new TextBox();
+            textBox_hpBencana = new TextBox();
+            textBox_lembagaBencana = new TextBox();
+            pictureBox3 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonFace;
+            panel1.Controls.Add(textBox_deskripsiHospital);
+            panel1.Controls.Add(textBox_alamatHospital);
+            panel1.Controls.Add(textBox_tlpHospital);
+            panel1.Controls.Add(textBox_lembagaHospital);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(102, 88);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1264, 240);
+            panel1.TabIndex = 0;
+            // 
+            // textBox_deskripsiHospital
+            // 
+            textBox_deskripsiHospital.BorderStyle = BorderStyle.FixedSingle;
+            textBox_deskripsiHospital.Location = new Point(374, 174);
+            textBox_deskripsiHospital.Name = "textBox_deskripsiHospital";
+            textBox_deskripsiHospital.Size = new Size(850, 27);
+            textBox_deskripsiHospital.TabIndex = 1;
+            textBox_deskripsiHospital.Text = "Deskripsi";
+            textBox_deskripsiHospital.TextAlign = HorizontalAlignment.Center;
+            textBox_deskripsiHospital.TextChanged += textBox_deskripsiHospital_TextChanged;
+            // 
+            // textBox_alamatHospital
+            // 
+            textBox_alamatHospital.BorderStyle = BorderStyle.FixedSingle;
+            textBox_alamatHospital.Location = new Point(374, 128);
+            textBox_alamatHospital.Name = "textBox_alamatHospital";
+            textBox_alamatHospital.Size = new Size(850, 27);
+            textBox_alamatHospital.TabIndex = 1;
+            textBox_alamatHospital.Text = "Alamat";
+            textBox_alamatHospital.TextAlign = HorizontalAlignment.Center;
+            textBox_alamatHospital.TextChanged += textBox_alamatHospital_TextChanged;
+            // 
+            // textBox_tlpHospital
+            // 
+            textBox_tlpHospital.BorderStyle = BorderStyle.FixedSingle;
+            textBox_tlpHospital.Location = new Point(374, 85);
+            textBox_tlpHospital.Name = "textBox_tlpHospital";
+            textBox_tlpHospital.Size = new Size(850, 27);
+            textBox_tlpHospital.TabIndex = 1;
+            textBox_tlpHospital.Text = "No Telpon";
+            textBox_tlpHospital.TextAlign = HorizontalAlignment.Center;
+            textBox_tlpHospital.TextChanged += textBox_tlpHospital_TextChanged;
+            // 
+            // textBox_lembagaHospital
+            // 
+            textBox_lembagaHospital.BorderStyle = BorderStyle.FixedSingle;
+            textBox_lembagaHospital.Location = new Point(374, 39);
+            textBox_lembagaHospital.Name = "textBox_lembagaHospital";
+            textBox_lembagaHospital.Size = new Size(850, 27);
+            textBox_lembagaHospital.TabIndex = 1;
+            textBox_lembagaHospital.Text = "Nama Lembaga";
+            textBox_lembagaHospital.TextAlign = HorizontalAlignment.Center;
+            textBox_lembagaHospital.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(330, 240);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonFace;
+            panel2.Controls.Add(textBox_deskripsiPolisi);
+            panel2.Controls.Add(textBox_alamatPolisi);
+            panel2.Controls.Add(textBox_hpPolisi);
+            panel2.Controls.Add(textBox_lembagaPolisi);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Location = new Point(102, 387);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1264, 240);
+            panel2.TabIndex = 0;
+            // 
+            // textBox_deskripsiPolisi
+            // 
+            textBox_deskripsiPolisi.BorderStyle = BorderStyle.FixedSingle;
+            textBox_deskripsiPolisi.Location = new Point(374, 174);
+            textBox_deskripsiPolisi.Name = "textBox_deskripsiPolisi";
+            textBox_deskripsiPolisi.Size = new Size(850, 27);
+            textBox_deskripsiPolisi.TabIndex = 1;
+            textBox_deskripsiPolisi.Text = "Deskripsi";
+            textBox_deskripsiPolisi.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox_alamatPolisi
+            // 
+            textBox_alamatPolisi.BorderStyle = BorderStyle.FixedSingle;
+            textBox_alamatPolisi.Location = new Point(374, 128);
+            textBox_alamatPolisi.Name = "textBox_alamatPolisi";
+            textBox_alamatPolisi.Size = new Size(850, 27);
+            textBox_alamatPolisi.TabIndex = 1;
+            textBox_alamatPolisi.Text = "Alamat";
+            textBox_alamatPolisi.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox_hpPolisi
+            // 
+            textBox_hpPolisi.BorderStyle = BorderStyle.FixedSingle;
+            textBox_hpPolisi.Location = new Point(374, 85);
+            textBox_hpPolisi.Name = "textBox_hpPolisi";
+            textBox_hpPolisi.Size = new Size(850, 27);
+            textBox_hpPolisi.TabIndex = 1;
+            textBox_hpPolisi.Text = "No Telpon";
+            textBox_hpPolisi.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox_lembagaPolisi
+            // 
+            textBox_lembagaPolisi.BorderStyle = BorderStyle.FixedSingle;
+            textBox_lembagaPolisi.Location = new Point(374, 39);
+            textBox_lembagaPolisi.Name = "textBox_lembagaPolisi";
+            textBox_lembagaPolisi.Size = new Size(850, 27);
+            textBox_lembagaPolisi.TabIndex = 1;
+            textBox_lembagaPolisi.Text = "Nama Lembaga";
+            textBox_lembagaPolisi.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Left;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(330, 240);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonFace;
+            panel3.Controls.Add(textBox_deskripsiBencana);
+            panel3.Controls.Add(textBox_alamatBencana);
+            panel3.Controls.Add(textBox_hpBencana);
+            panel3.Controls.Add(textBox_lembagaBencana);
+            panel3.Controls.Add(pictureBox3);
+            panel3.Location = new Point(102, 693);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1264, 240);
+            panel3.TabIndex = 0;
+            // 
+            // textBox_deskripsiBencana
+            // 
+            textBox_deskripsiBencana.BorderStyle = BorderStyle.FixedSingle;
+            textBox_deskripsiBencana.Location = new Point(374, 174);
+            textBox_deskripsiBencana.Name = "textBox_deskripsiBencana";
+            textBox_deskripsiBencana.Size = new Size(850, 27);
+            textBox_deskripsiBencana.TabIndex = 1;
+            textBox_deskripsiBencana.Text = "Deskripsi";
+            textBox_deskripsiBencana.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox_alamatBencana
+            // 
+            textBox_alamatBencana.BorderStyle = BorderStyle.FixedSingle;
+            textBox_alamatBencana.Location = new Point(374, 128);
+            textBox_alamatBencana.Name = "textBox_alamatBencana";
+            textBox_alamatBencana.Size = new Size(850, 27);
+            textBox_alamatBencana.TabIndex = 1;
+            textBox_alamatBencana.Text = "Alamat";
+            textBox_alamatBencana.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox_hpBencana
+            // 
+            textBox_hpBencana.BorderStyle = BorderStyle.FixedSingle;
+            textBox_hpBencana.Location = new Point(374, 85);
+            textBox_hpBencana.Name = "textBox_hpBencana";
+            textBox_hpBencana.Size = new Size(850, 27);
+            textBox_hpBencana.TabIndex = 1;
+            textBox_hpBencana.Text = "No Telpon";
+            textBox_hpBencana.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox_lembagaBencana
+            // 
+            textBox_lembagaBencana.BorderStyle = BorderStyle.FixedSingle;
+            textBox_lembagaBencana.Location = new Point(374, 39);
+            textBox_lembagaBencana.Name = "textBox_lembagaBencana";
+            textBox_lembagaBencana.Size = new Size(850, 27);
+            textBox_lembagaBencana.TabIndex = 1;
+            textBox_lembagaBencana.Text = "Nama Lembaga";
+            textBox_lembagaBencana.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Dock = DockStyle.Left;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(0, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(330, 240);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // detail_dataKeselamatan
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1422, 977);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "detail_dataKeselamatan";
+            Text = "detail_dataKeselamatan";
+            Load += detail_dataKeselamatan_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox_deskripsiHospital;
+        private TextBox textBox_alamatHospital;
+        private TextBox textBox_tlpHospital;
+        private TextBox textBox_lembagaHospital;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private TextBox textBox_deskripsiPolisi;
+        private TextBox textBox_alamatPolisi;
+        private TextBox textBox_hpPolisi;
+        private TextBox textBox_lembagaPolisi;
+        private PictureBox pictureBox2;
+        private Panel panel3;
+        private TextBox textBox_deskripsiBencana;
+        private TextBox textBox_alamatBencana;
+        private TextBox textBox_hpBencana;
+        private TextBox textBox_lembagaBencana;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ frontendpbo/detail_dataKeselamatan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace frontendpbo
+{
+    public partial class detail_dataKeselamatan : Form
+    {
+        public detail_dataKeselamatan()
+        {
+            InitializeComponent();
+        }
+
+        private void detail_dataKeselamatan_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            if (textBox2.TextLength == 0)
+            {
+                label2.Visible = true;
+            }
+        }
+
+        private void textBox_tlpHospital_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            if (textBox2.TextLength == 0)
+            {
+                label2.Visible = true;
+            }
+        }
+
+        private void textBox_alamatHospital_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            if (textBox2.TextLength == 0)
+            {
+                label2.Visible = true;
+            }
+        }
+
+        private void textBox_deskripsiHospital_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            if (textBox2.TextLength == 0)
+            {
+                label2.Visible = true;
+            }
+        }
+    }
+}
