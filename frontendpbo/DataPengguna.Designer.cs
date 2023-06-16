5cb2944c2a7adb5d52d9b5799b466b65ac899075
NeoHamzah <amirulhamzah758@gmail.com>
2023-06-16 17:52:13 +0700

    Adding Form DataPengguna (Frontend)

--- frontendpbo/DataPengguna.Designer.cs
+++ frontendpbo/DataPengguna.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace frontendpbo
 {
-    partial class Form4
+    partial class DataPengguna
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form4";
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(111, 106, 248);
+            label1.Location = new Point(63, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(253, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Data Pengguna";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ActiveBorder;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(63, 107);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(781, 479);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(879, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(231, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Nama Lengkap Pengguna";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(111, 106, 248);
+            textBox1.Location = new Point(879, 147);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(408, 26);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(879, 224);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 24);
+            label3.TabIndex = 4;
+            label3.Text = "Email Pengguna";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(879, 337);
+            label4.Name = "label4";
+            label4.Size = new Size(97, 24);
+            label4.TabIndex = 5;
+            label4.Text = "Username";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(884, 449);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 24);
+            label5.TabIndex = 6;
+            label5.Text = "Password";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(111, 106, 248);
+            textBox2.Location = new Point(879, 262);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(408, 26);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(111, 106, 248);
+            textBox3.Location = new Point(879, 374);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(408, 26);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(111, 106, 248);
+            textBox4.Location = new Point(879, 485);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(408, 26);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.ActiveBorder;
+            textBox5.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(606, 66);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(238, 24);
+            textBox5.TabIndex = 10;
+            textBox5.Text = "Search Pengguna";
+            // 
+            // DataPengguna
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1350, 650);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DataPengguna";
+            Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
     }
 }
--- frontendpbo/DataPengguna.cs
+++ frontendpbo/DataPengguna.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace frontendpbo
 {
-    public partial class Form4 : Form
+    public partial class DataPengguna : Form
     {
-        public Form4()
+        public DataPengguna()
         {
             InitializeComponent();
         }
