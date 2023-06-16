4879ef8ea4370ae845d781af4c66d3fd4b129960
Angga Dwi Kurniawan <anggadwikurniawan6@gmail.com>
2023-06-17 00:52:01 +0700

    tambah Models Keamanan, Penginapan, Pengunjung, Sarana dan prasana, transportasi

---
+++ frontendpbo/Models/Transportasi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontendpbo.Models
+{
+    internal class Transportasi
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public int Wisata_ID { get; set; }
+    }
+}
