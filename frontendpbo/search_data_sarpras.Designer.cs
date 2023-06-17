ac1cc3a70b6d1101e1284ae25793a9b433f05792
Akbaradityafirmansyah <akbar04.eiger@gmail.com>
2023-06-17 14:28:52 +0700

    pencarian data sarana prasarana

---
+++ frontendpbo/search_data_sarpras.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace frontendpbo
+{
+    partial class search_data_sarpras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeight = 29;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
+            dataGridView1.Location = new Point(20, 16);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(551, 338);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "id_sarana";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Nama Sarana";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Deskripsi Sarana";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "id_wisata";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.ReadOnly = true;
+            Column4.Width = 125;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(296, 218);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(551, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(171, 206);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 37);
+            label1.TabIndex = 2;
+            label1.Text = "Search";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gray;
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(276, 272);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(587, 372);
+            panel1.TabIndex = 3;
+            // 
+            // search_data_sarpras
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1051, 752);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "search_data_sarpras";
+            Text = "search_data_sarpras";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private TextBox textBox1;
+        private Label label1;
+        private Panel panel1;
+    }
+}
---
+++ frontendpbo/search_data_sarpras.cs
@@ -0,0 +1,51 @@
+﻿using frontendpbo.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frontendpbo
+{
+    public partial class search_data_sarpras : Form
+    {
+        DatabaseHelper db;
+        public search_data_sarpras()
+        {
+            InitializeComponent();
+            ShowDataSarpras();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var db = new DatabaseHelper();
+            var reader = db.Select($"SELECT * FROM sarana_prasarana WHERE nama_sarana LIKE '%{textBox1.Text}%'");
+            dataGridView1.Rows.Clear();
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add((int)reader["id_sarana"], (string)reader["nama_sarana"], (string)reader["deskripsi_saranat"], (int)reader["wisata_idt"]);
+            }
+            reader.Close();
+        }
+
+        private void ShowDataSarpras()
+        {
+            var db = new DatabaseHelper();
+            var reader = db.Select("SELECT id_sarana,nama_sarana,deskripsi_sarana,wisata_id FROM public.sarana_prasarana;");
+            dataGridView1.Rows.Clear();
+
+
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add((int)reader["id_sarana"], (string)reader["nama_sarana"], (string)reader["deskripsi_saranat"], (int)reader["wisata_idt"]);
+            }
+
+            reader.Close();
+
+        }
+    }
+}
