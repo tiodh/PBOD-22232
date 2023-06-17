d9a4e65713bc488575038852d341e90aae1f7740
Angga Dwi Kurniawan <anggadwikurniawan6@gmail.com>
2023-06-17 18:29:14 +0700

    menambahkan model event, pengumuman, riwayat kunjungan, tempat makan, tiket, ulasan, dan umkm

---
+++ frontendpbo/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontendpbo.Models
+{
+    internal class Event
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Deskripsi { get; set; }
+        public DateTime Tanggal_Event { get; set; }
+        public int Wisata_ID { get; set; }
+    }
+}
