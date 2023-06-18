8743bf28ad44137417d6f841c97b52dc504c7cad
Alyadnj <alyadwinurjanah@gmail.com>
2023-06-18 21:02:02 +0700

    Menambah metode"tambah/create" data pada fitur data_keselamatan

--- frontendpbo/CRUD_dataKeselamatan.cs
+++ frontendpbo/CRUD_dataKeselamatan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,6 +26,31 @@ namespace frontendpbo
         private void ReadData()
         {
 
+        }
+
+        private void Tambah_Keselamatan_Click(object sender, EventArgs e)
+        {
+                using (NpgsqlConnection connection = new NpgsqlConnection("host=localhost;port=5432;database=DummyDatabase;user id=postgres;password=220305"))
+                {
+                    connection.Open();
+                    NpgsqlCommand command = connection.CreateCommand();
+                    command.Connection = connection;
+                    command.CommandText = "insert into Keamanan(nama_keamanan, deskripsi_keamanan, alamat_keamanan, no_tlp) values(@nama_keamanan, @deskripsi_keamanan, @alamat_keamanan, @no_tlp)";
+                    command.Parameters.Add(new NpgsqlParameter("@nama_keamanan", textBox_nama_lembaga_dataKeamanan.Text));
+                    command.Parameters.Add(new NpgsqlParameter("@no_tlp", textBoxnoHp_dataKeamanan.Text));
+                    command.Parameters.Add(new NpgsqlParameter("@alamat_keamanan", textBoxalamat_dataKeamanan.Text));
+                    command.Parameters.Add(new NpgsqlParameter("@deskripsi_keamanan", textBoxdeskripsi_dataKeamanan.Text));
+
+                    textBox_nama_lembaga_dataKeamanan.Text = "";
+                    textBoxnoHp_dataKeamanan.Text = "";
+                    textBoxalamat_dataKeamanan.Text = "";
+                    textBoxdeskripsi_dataKeamanan.Text = "";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    MessageBox.Show("Data berhasil diinput");
+                    //RefreshUlasanTerakhir();
+                    //RefreshRatingTerakhir();
+                }
         }
     }
 }
