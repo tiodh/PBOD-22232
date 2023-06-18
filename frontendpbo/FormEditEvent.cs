d1dc976b8547496a73e45e4f22a0d7d30cc1bd1c
riofadil9 <riofadil9@gmail.com>
2023-06-19 06:06:11 +0700

    in FormEditEvent,Connecting Database, make load gridview method, and delete method

--- frontendpbo/FormEditEvent.cs
+++ frontendpbo/FormEditEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +16,99 @@ namespace frontendpbo
         public FormEditEvent()
         {
             InitializeComponent();
+            LoadData();
         }
+        private void LoadData()
+        {
+            string connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=Rio09rio:v;Database=DataEvent";
+            string query = "SELECT * FROM eventacara";
 
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                {
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+        }
         private void FormEditEvent_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCreateEvent_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnUpdateEvent_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDeleteEvent_Click(object sender, EventArgs e)
+        {
+            int id_event = int.Parse(tbIdEvent.Text);
+            Crud.DeleteData(id_event);
+            LoadData();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    class Crud
+    {
+        public static void DeleteData(int kode)
+        {
+            Connec connectdb = new Connec();
+            string querydelete = $"delete from eventacara where id_event = {kode}::integer;;";
+            connectdb.Run(querydelete);
+        }
+
+    }
+    class Connec
+    {
+        public NpgsqlConnection connect;
+
+        public Connec()
+        {
+            NpgsqlConnection connect = new NpgsqlConnection();
+            connect.ConnectionString = "Server=localhost;Port=5432;User Id=postgres;Password=Rio09rio:v;Database=DataEvent";
+        }
+
+        public DataTable Run(string sql)
+        {
+            NpgsqlConnection connect = new NpgsqlConnection();
+            connect.ConnectionString = "Server=localhost;Port=5432;User Id=postgres;Password=Rio09rio:v;Database=DataEvent";
+
+            DataTable dt = new DataTable();
+            try
+            {
+                connect.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connect;
+                cmd.CommandText = sql;
+                cmd.CommandType = CommandType.Text;
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                da.Fill(dt);
+                cmd.Dispose();
+                connect.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+            return dt;
         }
     }
 }
