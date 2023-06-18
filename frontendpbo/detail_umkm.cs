f72da883f257fb7db80369194deb668ce72552e6
Razdiablo <ramadhanibudiarto@gmail.com>
2023-06-19 03:52:01 +0700

    tambah detail umkm dan jenis umkm

---
+++ frontendpbo/detail_umkm.cs
@@ -0,0 +1,56 @@
+namespace tampil_data_umkm
+{
+    public partial class detail_umkm : Form
+    {
+        public detail_umkm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label25_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Second_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ frontendpbo/jenis_umkm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace tampil_data_umkm
+{
+    partial class jenis_umkm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Cambria", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(301, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(542, 52);
+            label1.TabIndex = 2;
+            label1.Text = "Jenis-Jenis UMKM";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveBorder;
+            pictureBox1.Location = new Point(72, 106);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(207, 143);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.ActiveBorder;
+            pictureBox2.Location = new Point(347, 106);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(207, 143);
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.ActiveBorder;
+            pictureBox3.Location = new Point(624, 106);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(207, 143);
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.ActiveBorder;
+            pictureBox4.Location = new Point(911, 106);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(207, 143);
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.ActiveBorder;
+            pictureBox5.Location = new Point(911, 333);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(207, 143);
+            pictureBox5.TabIndex = 10;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.ActiveBorder;
+            pictureBox6.Location = new Point(624, 333);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(207, 143);
+            pictureBox6.TabIndex = 9;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = SystemColors.ActiveBorder;
+            pictureBox7.Location = new Point(347, 333);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(207, 143);
+            pictureBox7.TabIndex = 8;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = SystemColors.ActiveBorder;
+            pictureBox8.Location = new Point(72, 333);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(207, 143);
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1088, 505);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 35);
+            button1.TabIndex = 11;
+            button1.Text = "Detail Umkm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // jenis_umkm
+            // 
+            AutoScaleDimensions = new SizeF(6F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkViolet;
+            ClientSize = new Size(1197, 552);
+            Controls.Add(button1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Font = new Font("Arial Narrow", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "jenis_umkm";
+            Text = "Form2";
+            Load += first_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private Button button1;
+    }
+}
