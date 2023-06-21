d6a72f3d2d99fc3626e96cd7263a3fb53554e226
Zuruu12 <rayhannavis11@gmail.com>
2023-06-22 02:06:54 +0700

    OOP CREATE PENGGUNA

--- frontendpbo/Contexts/ContextPengguna.cs
+++ frontendpbo/Contexts/ContextPengguna.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using frontendpbo.Models;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,34 @@ namespace frontendpbo.Contexts
 {
     internal class ContextPengguna
     {
+        public List<Pengguna> listPengguna = new List<Pengguna>() { };
+        Pengguna pengguna = new Pengguna();
+        public bool Create(Pengguna pengguna)
+        {
+            bool isSuccess = false;
+            string conStr = "Server=localhost;Port=5432;User Id=postgres;Password=123;Database=peta_jember;";
+            using (NpgsqlConnection conn = new NpgsqlConnection(conStr))
+            {
+                string sql =
+                    "INSERT INTO pengguna(nama_lengkap, email, username, password) values (:nama_lengkap,:email,:username,:password)";
+                conn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.Add(new NpgsqlParameter(":nama_lengkap", pengguna.Nama_Lengkap));
+                    cmd.Parameters.Add(new NpgsqlParameter(":email", pengguna.Email));
+                    cmd.Parameters.Add(new NpgsqlParameter(":username", pengguna.Username));
+                    cmd.Parameters.Add(new NpgsqlParameter(":password", pengguna.Password));
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    int newdata = cmd.ExecuteNonQuery();
+                    if (newdata > 0)
+                    {
+                        isSuccess = true;
+                        this.listPengguna.Add(pengguna);
+                    }
+                }
+            }
+            return isSuccess;
+        }
     }
 }
