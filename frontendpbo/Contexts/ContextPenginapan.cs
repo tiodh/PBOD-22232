02fec07bc6e7c41d08f07695dca60ea7fabfb309
kris15putrach <krisnapr43@gmail.com>
2023-06-22 22:32:46 +0700

    fixing pnginapan

--- frontendpbo/Contexts/ContextPenginapan.cs
+++ frontendpbo/Contexts/ContextPenginapan.cs
@@ -11,7 +11,10 @@ namespace frontendpbo.Contexts
     internal class ContextPenginapan
     {
         public List<Penginapan> listPenginapan = new List<Penginapan>() { };
-        Penginapan penginapan = new Penginapan();
+        public List<Penginapan> GetListPeninapan()
+        {
+            return listPenginapan;
+        }
 
         public bool Insert(Penginapan penginapan)
         {
@@ -66,47 +69,79 @@ namespace frontendpbo.Contexts
                         listPenginapan.Add(penginapan);
                     }
                 }
+                return isSuccess;
             }
-            return isSuccess;
+            
         }
-    }
-    public bool Update(Penginapan penginapan)
-    {
-        bool isSuccess = false;
-
-        string conStr = "Server=localhost;Port=5432;User Id=postgres;Password=dewi2493;Database=peta_jember;";
-        using (NpgsqlConnection conn = new NpgsqlConnection(conStr))
+        public bool Update(Penginapan penginapan)
         {
-            string sql = @"UPDATE SET id_penginapan = :id_penginapan, nama_penginapan = :nama_penginapan,deskripsi_penginapan = :deskripsi_penginapan where id_wisata = :id_wisata";
-            conn.Open();
-            using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
-            {
-                cmd.Parameters.Add(new NpgsqlParameter(":id_penginapan", Penginapan.ID));
-                cmd.Parameters.Add(new NpgsqlParameter(":nama_penginapan", Penginapan.Name));
-                cmd.Parameters.Add(new NpgsqlParameter(":deskripsi_penginapan", Penginapan.Description));
-                cmd.Parameters.Add(new NpgsqlParameter(":wisata_id", Penginapan.Wisata_ID));
+            bool isSuccess = false;
 
-                cmd.CommandType = System.Data.CommandType.Text;
-                int JumlahData = cmd.ExecuteNonQuery();
-                if (JumlahData > 0)
+            string conStr = "Server=localhost;Port=5432;User Id=postgres;Password=dewi2493;Database=peta_jember;";
+            using (NpgsqlConnection conn = new NpgsqlConnection(conStr))
+            {
+                string sql = "UPDATE SET id_penginapan = :id_penginapan, nama_penginapan = :nama_penginapan,deskripsi_penginapan = :deskripsi_penginapan where id_wisata = :id_wisata";
+                conn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
                 {
-                    isSuccess = true;
-                    foreach (var temp in this.listWisata)
+                    cmd.Parameters.Add(new NpgsqlParameter(":id_penginapan", penginapan.ID));
+                    cmd.Parameters.Add(new NpgsqlParameter(":nama_penginapan", penginapan.Name));
+                    cmd.Parameters.Add(new NpgsqlParameter(":deskripsi_penginapan", penginapan.Description));
+                    cmd.Parameters.Add(new NpgsqlParameter(":wisata_id", penginapan.Wisata_ID));
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    int JumlahData = cmd.ExecuteNonQuery();
+                    if (JumlahData > 0)
                     {
-                        var t = temp as Penginapan;
-                        if (t != null && t.Id_Penginapan.Equals(penginapan.Id_penginapan))
+                        isSuccess = true;
+                        foreach (var temp in this.listPenginapan)
                         {
-                            t.ID = penginapan.ID;
-                            t.Name = penginapan.Nama_Penginapan;
-                            t.Description = penginapan.Description;
-                            t.Wisata_ID = penginapan.Wisata_ID;
+                            var t = temp as Penginapan;
+                            if (t != null && t.ID.Equals(penginapan.ID))
+                            {
+                                t.ID = penginapan.ID;
+                                t.Name = penginapan.Name;
+                                t.Description = penginapan.Description;
+                                t.Wisata_ID = penginapan.Wisata_ID;
+                            }
                         }
                     }
+                    MessageBox.Show("Sukses Mengupdate Data");
                 }
-                MessageBox.Show("Sukses Mengupdate Data");
+                return isSuccess;
             }
-
+            
         }
-        return isSuccess;
+        public List<Penginapan> Search(string query)
+        {
+            List<Penginapan> searchResults = new List<Penginapan>();
+
+            string conStr = "Server=localhost;Port=5432;User Id=postgres;Password=123;Database=peta_jember";
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(conStr))
+            {
+                string sql = "SELECT * FROM Tiket WHERE nama_penginapan ILIKE '%@query%' or deskripsi_penginapan ILIKE'%@query%'";
+
+                conn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@query", "%" + query + "%");
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Penginapan penginapan = new Penginapan();
+                        penginapan.ID = (int)reader["id_penginapan"];
+                        penginapan.Name = (string)reader["nama_penginapan"];
+                        penginapan.Description = (string)reader["deskripsi_penginapan"];
+                        searchResults.Add(penginapan);
+                    }
+                }
+            }
+            return searchResults;
+        }
+   
+
+
     }
-}
