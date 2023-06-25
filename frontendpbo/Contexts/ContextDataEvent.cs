f7a4838c950ffda1e20c0913ede0a2d5efcb72c1
martri327 <134029959+martri327@users.noreply.github.com>
2023-06-25 22:00:31 +0700

    CRUD Event

--- frontendpbo/Contexts/ContextDataEvent.cs
+++ frontendpbo/Contexts/ContextDataEvent.cs
@@ -18,7 +18,7 @@ namespace frontendpbo.Contexts
 
             using (NpgsqlConnection conn = new NpgsqlConnection(connec))
             {
-                string sql = @"SELECT * FROM event_acara;";
+                string sql = @"SELECT * FROM event_acara order by id_event";
 
                 conn.Open();
                 using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
@@ -60,120 +60,128 @@ namespace frontendpbo.Contexts
         {
             bool isSucces = false;
             string connec = "Server=localhost;Port=5432;User Id=postgres;Password=123;Database=peta_jember";
-            using (NpgsqlConnection conn = new NpgsqlConnection(connec))
+            try
             {
-                string sql = @"INSERT INTO public.event_acara(id_event, nama_event, deskripsi_event, tanggal_event, wisata_id)
-	                              VALUES (:id_event,:nama_event,:deskripsi_event, :tanggal_event, :wisata_id);";
-                conn.Open();
-
-                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                using (NpgsqlConnection conn = new NpgsqlConnection(connec))
                 {
-                    cmd.Parameters.Add(new NpgsqlParameter(":id_event", dataEvent.Id));
-                    cmd.Parameters.Add(new NpgsqlParameter(":nama_event", dataEvent.Nama));
-                    cmd.Parameters.Add(new NpgsqlParameter(":deskripsi_event", dataEvent.Deskripsi));
-                    cmd.Parameters.Add(new NpgsqlParameter(":tanggal_event", dataEvent.Tanggal_Event));
-                    cmd.Parameters.Add(new NpgsqlParameter(":wisata_id", dataEvent.Wisata_ID));
+                    string sql = @"INSERT INTO public.event_acara(id_event, nama_event, deskripsi_event, tanggal_event, wisata_id)
+	                                  VALUES (:id_event,:nama_event,:deskripsi_event, :tanggal_event, :wisata_id);";
+                    conn.Open();
 
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    int jmldata = cmd.ExecuteNonQuery();
-                    if (jmldata > 0)
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
                     {
-                        isSucces = true;
-                        this.listDataEvent.Add(dataEvent);
-                        MessageBox.Show("Success");
+                        cmd.Parameters.Add(new NpgsqlParameter(":id_event", dataEvent.Id));
+                        cmd.Parameters.Add(new NpgsqlParameter(":nama_event", dataEvent.Nama));
+                        cmd.Parameters.Add(new NpgsqlParameter(":deskripsi_event", dataEvent.Deskripsi));
+                        cmd.Parameters.Add(new NpgsqlParameter(":tanggal_event", dataEvent.Tanggal_Event));
+                        cmd.Parameters.Add(new NpgsqlParameter(":wisata_id", dataEvent.Wisata_ID));
 
-
-
-
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        int jmldata = cmd.ExecuteNonQuery();
+                        if (jmldata > 0)
+                        {
+                            isSucces = true;
+                            this.listDataEvent.Add(dataEvent);
+                            MessageBox.Show("Success");
+                        }
                     }
-
-
-
                 }
-
-
+            }
+            catch (Exception ex){
+                MessageBox.Show("Tolong pastikan Input data dengan benar pada bagian Id");
             }
             return isSucces = Readdata();
         }
+
         public bool Update(Event dataEvent)
         {
             bool isSucces = false;
             string connec = "Server=localhost;Port=5432;User Id=postgres;Password=123;Database=peta_jember";
 
-            using (NpgsqlConnection conn = new NpgsqlConnection(connec))
+            try
             {
 
-                string sql = @"UPDATE event_acara SET nama_event = :nama_event, deskripsi_event = :deskripsi_event, wisata_id = :wisata_id WHERE id_Event = :id_Event;";
-
-                conn.Open();
-                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                using (NpgsqlConnection conn = new NpgsqlConnection(connec))
                 {
-                    cmd.CommandText = sql;
-                    cmd.Parameters.AddWithValue(":nama_event", dataEvent.Nama);
-                    cmd.Parameters.AddWithValue(":deskripsi_event", dataEvent.Deskripsi);
-                    cmd.Parameters.AddWithValue(":wisata_id", dataEvent.Wisata_ID);
-                    cmd.Parameters.AddWithValue(":id_Event", dataEvent.Id);
 
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-                    listDataEvent.Clear();
-                    while (reader.Read())
+                    string sql = @"UPDATE event_acara SET nama_event = :nama_event, deskripsi_event = :deskripsi_event, wisata_id = :wisata_id WHERE id_Event = :id_Event;";
+
+                    conn.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
                     {
-                        Event dataevent = new Event();
-                        dataEvent.Id = (int)reader["id_event"];
-                        dataEvent.Nama = (string)reader["nama_event"];
-                        dataEvent.Deskripsi = (string)reader["deskripsi_event"];
-                        dataEvent.Tanggal_Event = DateOnly.FromDateTime((DateTime)reader["tanggal_event"]);
-                        dataEvent.Wisata_ID = (int)reader["wisata_id"];
+                        cmd.CommandText = sql;
+                        cmd.Parameters.AddWithValue(":nama_event", dataEvent.Nama);
+                        cmd.Parameters.AddWithValue(":deskripsi_event", dataEvent.Deskripsi);
+                        cmd.Parameters.AddWithValue(":wisata_id", dataEvent.Wisata_ID);
+                        cmd.Parameters.AddWithValue(":id_Event", dataEvent.Id);
 
-                        int jmldata = cmd.ExecuteNonQuery();
-                        listDataEvent.Add(dataEvent);
-                        if (jmldata > 0)
+                        NpgsqlDataReader reader = cmd.ExecuteReader();
+                        listDataEvent.Clear();
+                        while (reader.Read())
                         {
-                            isSucces = true;
-                            MessageBox.Show("Success");
-                            Readdata();
+                            Event dataevent = new Event();
+                            dataEvent.Id = (int)reader["id_event"];
+                            dataEvent.Nama = (string)reader["nama_event"];
+                            dataEvent.Deskripsi = (string)reader["deskripsi_event"];
+                            dataEvent.Tanggal_Event = DateOnly.FromDateTime((DateTime)reader["tanggal_event"]);
+                            dataEvent.Wisata_ID = (int)reader["wisata_id"];
+
+                            int jmldata = cmd.ExecuteNonQuery();
+                            listDataEvent.Add(dataEvent);
+                            if (jmldata > 0)
+                            {
+                                isSucces = true;
+                                MessageBox.Show("Success");
+                                Readdata();
+                            }
+
+
                         }
-
-
                     }
+
+
                 }
+            }
 
-
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tolong pastikan Input data dengan benar pada bagian Id");
             }
             return isSucces = Readdata();
-        }
-        public void setDataEvent(Event dataEvent)
-        {
-
-
         }
         public bool deleted(Event dataEvent)
         {
             bool isSucces = false;
             string connec = "Server=localhost;Port=5432;User Id=postgres;Password=123;Database=peta_jember";
-            using (NpgsqlConnection conn = new NpgsqlConnection(connec))
+            try
             {
-                string sql = @"DELETE FROM event_acara WHERE id_event = :id_event;";
-                conn.Open();
-                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue(":id_event", dataEvent.Id);
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    int jmldata = cmd.ExecuteNonQuery();
-                    if (jmldata > 0)
-                    {
-                        isSucces = true;
-                        var itemToRemove = listDataEvent.Single(rec => rec.Id == dataEvent.Id);
-                        listDataEvent.Remove(itemToRemove);
-                    }
 
+                using (NpgsqlConnection conn = new NpgsqlConnection(connec))
+                {
+                    string sql = @"DELETE FROM event_acara WHERE id_event = :id_event;";
+                    conn.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue(":id_event", dataEvent.Id);
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        int jmldata = cmd.ExecuteNonQuery();
+                        if (jmldata > 0)
+                        {
+                            isSucces = true;
+                            var itemToRemove = listDataEvent.Single(rec => rec.Id == dataEvent.Id);
+                            listDataEvent.Remove(itemToRemove);
+                        }
+
+
+                    }
+                    MessageBox.Show("Success");
 
                 }
-                MessageBox.Show("Success");
+            }
 
-
-
-
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tolong pastikan Input data dengan benar pada bagian Id");
             }
             return isSucces = Readdata();
 
@@ -183,29 +191,37 @@ namespace frontendpbo.Contexts
             List<Event> searchResults = new List<Event>();
 
             string conStr = "Server=localhost;Port=5432;User Id=postgres;Password=123;Database=peta_jember";
-
-            using (NpgsqlConnection conn = new NpgsqlConnection(conStr))
+            try
             {
-                string sql = "SELECT * FROM event_acara WHERE nama_event ILIKE @query OR  deskripsi_event ILIKE @query";
 
-                conn.Open();
-                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                using (NpgsqlConnection conn = new NpgsqlConnection(conStr))
                 {
-                    cmd.Parameters.AddWithValue("@query", "%" + query + "%");
+                    string sql = "SELECT * FROM event_acara WHERE nama_event ILIKE @query OR  deskripsi_event ILIKE @query";
 
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
+                    conn.Open();
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
                     {
-                        Event even = new Event();
-                        even.Id = (int)reader["id_event"];
-                        even.Nama = (string)reader["nama_event"];
-                        even.Deskripsi = (string)reader["deskripsi_event"];
-                        even.Tanggal_Event = DateOnly.FromDateTime((DateTime)reader["tanggal_event"]);
-                        even.Wisata_ID = (int)reader["wisata_id"];
-                        searchResults.Add(even);
+                        cmd.Parameters.AddWithValue("@query", "%" + query + "%");
+
+                        NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            Event even = new Event();
+                            even.Id = (int)reader["id_event"];
+                            even.Nama = (string)reader["nama_event"];
+                            even.Deskripsi = (string)reader["deskripsi_event"];
+                            even.Tanggal_Event = DateOnly.FromDateTime((DateTime)reader["tanggal_event"]);
+                            even.Wisata_ID = (int)reader["wisata_id"];
+                            searchResults.Add(even);
+                        }
                     }
                 }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tolong pastikan Input data dengan benar pada bagian Id");
             }
             return searchResults;
         }
